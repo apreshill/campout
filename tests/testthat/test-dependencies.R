7061415a31049682b3cfb76397bafa7bb8ae7da7
Luke Johnston <lwjohnst@gmail.com>
2019-06-06 15:15:48 +0200

    forgot to remove

--- tests/testthat/test-dependencies.R
+++ tests/testthat/test-dependencies.R
@@ -5,17 +5,9 @@ req_file <- fs::dir_ls(output_dir, regexp = "requirements\\.R$", recurse = TRUE)
 test_that("dependencies are extracted", {
     actual_deps <- extract_rpkg_deps(req_file)
     expected_deps <- c(
-      "ggplot2",
       "dplyr",
       "tidyr",
-      "broom.mixed",
-      "forcats",
-      "dagitty",
-      "MuMIn",
-      "carpenter",
-      "lme4",
       "glue",
-      "knitr",
       "purrr",
       "stringr"
     )
