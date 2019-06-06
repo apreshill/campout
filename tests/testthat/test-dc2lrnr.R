ddaa2191d3ad22d48c3e5ab5a8d02710ab7a4a8c
Luke Johnston <lwjohnst@gmail.com>
2019-06-06 15:16:03 +0200

    test for conversion to lrnr

---
+++ tests/testthat/test-dc2lrnr.R
@@ -0,0 +1,9 @@
+context("Convert a DC repo into a learnr package")
+
+dc_repo <- fs::path(output_dir, "dc-course")
+lrnr_pkg_name <- "acd.course"
+lrnr_pkg <- fs::path(output_dir, lrnr_pkg_name)
+
+test_that("DataCamp repo is converted to a package", {
+  datacamp_to_learnr_pkg(dc_repo, lrnr_pkg, "TEST RUN")
+})
